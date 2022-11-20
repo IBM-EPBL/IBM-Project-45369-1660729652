2fae52a0774eb029199b577f389affee4887e1c3
Praveen kumar M <113007644+Praveenkumar2803@users.noreply.github.com>
2022-11-20 14:00:03 +0530

    Update sendgrid.py

--- Project Development Phase/Sprint-3/sendgrid.py
+++ Project Development Phase/Sprint-3/sendgrid.py
@@ -3,13 +3,13 @@ from sendgrid.helpers.mail import Mail
 
 
 message = Mail(
-    from_email="plasmadonorbank@gmail.com",
-    to_emails="arun1751121@gmail.com",
+    from_email="FROMMAIL",
+    to_emails="TOMAIL",
     subject='Plasma Donor',
     html_content='<p>Hello, Your Registration was successfull. <br><br> Thank you for choosing us.</p>')
 
 sg = SendGridAPIClient(
-    api_key='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')
+    api_key='SENDGRID_API_KEY')
 
 response = sg.send(message)
 print(response.status_code, response.body)
